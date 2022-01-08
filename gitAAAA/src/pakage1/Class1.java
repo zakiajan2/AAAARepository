7fb2745f69898d63729e33d5140844e5cbf20dc0
zakiajan2 <zakieh.mohamadi@gmail.com>
2022-01-08 10:12:33 -0500

    pull reguest

--- gitAAAA/src/pakage1/Class1.java
+++ gitAAAA/src/pakage1/Class1.java
@@ -12,5 +12,9 @@ public class Class1 {
 		 */
 		System.out.println("every changes in repo we need commit there too to be able to pull here");
 System.out.println("to creat new branch go to team- switch- put name- config-merge");
+System.out.println(" name of new branch is SDETA");
+System.out.println(" by first commit and push a actually push codes to SDETA branch ");
+System.out.println("i need create pull request to push may change from sdet a branch to main branch");
+String moveToMain="to move changes to main repo in git hub we have to go to github account and make pull Request there";
 	}
 }
