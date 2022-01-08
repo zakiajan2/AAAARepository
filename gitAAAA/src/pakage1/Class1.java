28f5f8477f0923d93ef247f7f9d75a588e2687c9
zakiajan2 <zakieh.mohamadi@gmail.com>
2022-01-08 12:54:13 -0500

    sdetA commit

--- gitAAAA/src/pakage1/Class1.java
+++ gitAAAA/src/pakage1/Class1.java
@@ -5,13 +5,13 @@ public class Class1 {
 
 		/*
 		 * stage of making git hub repository - create new repo -follow instructions
-		 * -save the url you need in eclips in eclips after creating a projece you need
+		 * -save the url you need in eclips in eclips after creating a project you need
 		 * add new changes to staged area -then write a commit - commit button then you
 		 * need enter URL of git hub there - then host and other needed tabs will refill
 		 * automatcally - then finish
 		 */
 		System.out.println("every changes in repo we need commit there too to be able to pull here");
-System.out.println("to creat new branch go to team- switch- put name- config-merge");
+System.out.println("to creat new branch go toproject team- switch- put name- config-merge" +"or repository switch");
 System.out.println(" name of new branch is SDETA");
 System.out.println(" by first commit and push a actually push codes to SDETA branch ");
 System.out.println("i need create pull request to push may change from sdet a branch to main branch");
