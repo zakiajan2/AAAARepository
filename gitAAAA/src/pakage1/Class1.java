c3c014fbf0f63dd8b5961d1a1060e2a69dfc88bf
zakiajan2 <97071702+zakiajan2@users.noreply.github.com>
2022-01-08 09:51:02 -0500

    Update Class1.java
    changes from repo

--- gitAAAA/src/pakage1/Class1.java
+++ gitAAAA/src/pakage1/Class1.java
@@ -1,5 +1,14 @@
 package pakage1;
 
 public class Class1 {
-//stage of making git hub repository
+/*stage of making git hub repository
+  - create new repo
+    -follow instructions
+    -save the url
+    you need in eclips
+    in eclips after creating a projece you need add new changes to staged area
+    -then write a commit
+    - commit button then you need enter URL of git hub there
+    - then host and other needed tabs will refill automatcally
+   - then finish*/
 }
