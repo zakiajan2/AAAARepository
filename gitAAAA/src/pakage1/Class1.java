5d977a37736480b42df7e9605db579a41b43ac32
zakiajan2 <zakieh.mohamadi@gmail.com>
2022-01-08 14:02:01 -0500

    commit after see conflict

--- gitAAAA/src/pakage1/Class1.java
+++ gitAAAA/src/pakage1/Class1.java
@@ -17,10 +17,7 @@ System.out.println(" by first commit and push a actually push codes to SDETA bra
 System.out.println("i need create pull request to push may change from sdet a branch to main branch");
 String moveToMain="to move changes to main repo in git hub we have to go to github account and make pull Request there";
 	String remember="at job it is better first pull to see changes in main then push your code to main";
-<<<<<<< HEAD
 	//new change
-=======
 	//to create new branch SDWTB : project-team-switch-SDETB
->>>>>>> master
 	}
 }
