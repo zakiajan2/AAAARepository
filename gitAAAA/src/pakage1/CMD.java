d71f877e64e9f688942ae5b63274d0bc00f0d135
zakiajan2 <zakieh.mohamadi@gmail.com>
2022-01-08 14:27:06 -0500

    commitbb

--- gitAAAA/src/pakage1/CMD.java
+++ gitAAAA/src/pakage1/CMD.java
@@ -5,6 +5,7 @@ public class CMD {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //IT Was new class creat by sdet B he did pull request to send this change to main class by using github
+
 	}
 
 }
