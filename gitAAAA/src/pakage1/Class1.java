e08b0f513769582b8b3d8781b05ce4a5dc29e0f6
zakiajan2 <zakieh.mohamadi@gmail.com>
2022-01-08 09:56:36 -0500

    change from project

--- gitAAAA/src/pakage1/Class1.java
+++ gitAAAA/src/pakage1/Class1.java
@@ -1,14 +1,16 @@
 package pakage1;
 
 public class Class1 {
-/*stage of making git hub repository
-  - create new repo
-    -follow instructions
-    -save the url
-    you need in eclips
-    in eclips after creating a projece you need add new changes to staged area
-    -then write a commit
-    - commit button then you need enter URL of git hub there
-    - then host and other needed tabs will refill automatcally
-   - then finish*/
+	public static void main(String[] args) {
+
+		/*
+		 * stage of making git hub repository - create new repo -follow instructions
+		 * -save the url you need in eclips in eclips after creating a projece you need
+		 * add new changes to staged area -then write a commit - commit button then you
+		 * need enter URL of git hub there - then host and other needed tabs will refill
+		 * automatcally - then finish
+		 */
+		System.out.println("every changes in repo we need commit there too to be able to pull here");
+
+	}
 }
